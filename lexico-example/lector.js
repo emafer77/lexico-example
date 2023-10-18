9be426a96ee248892a4e7217c02795dd00ba96b4
Emafer77 <al18760630@ite.edu.mx>
2023-10-18 00:25:16 -0700

    actulizar  arreglos operadoresLogicos

--- lexico-example/lector.js
+++ lexico-example/lector.js
@@ -20,8 +20,7 @@ fs.readFile('database.txt', 'utf8', (err, data) => {
        
        var query = cadena_split[i].split(" ");}
 
-       const operadoresLogicos = [  "AND", "OR", "NOT"];
-       const operadoresComparacion = [">", "<", "=",">=", "<=", "!="];
+   
        const renglonesIncorrectos = [];
 
 
